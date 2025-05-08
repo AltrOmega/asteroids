53c5014b2a601292115e9ed09a48f0d5798149c7
AltrOmega <michal.szalachowski.33@gmail.com>
2025-05-08 08:39:49 +0200

    feat: add delta time and frame cap

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from constants import *
 
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
- 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,6 +14,7 @@ def main():
 
         screen.fill('#000000')
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
